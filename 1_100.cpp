1b01dd60b2ea9c7670b8c759b760639c595f0fe8
Diego Hurtado <164244994+diegohurtado925@users.noreply.github.com>
2024-05-25 20:28:20 -0600

    Add files via upload

---
+++ 1_100.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int numero = 0;
+    for (int i = 0; i<=100; i++)
+    {
+        numero +=i;
+    }
+    cout << numero;
+
+return 0;
+}
